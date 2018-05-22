6dda1aec1f4ca05efc3ea1a07ff55c0f5bc4c49c
IXOFIXOF <IXOF@POCZTA.ONET.PL>
2018-05-22 23:33:17 +0200

    Init commit

---
+++ Leki/Leki.cpp
@@ -0,0 +1,13 @@
+// Leki.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << "Klasy bazowe leki, choroby, klasy pochodne zio³a, antybiotyki, placebo np. homeopatia, magia, schorzenia uk³adu trawiennego, oczu itd. Przeci¹¿yæ operator strumienia i dodawania i odejmowania leków z leczonych chorób oraz parê operatorów wed³ug w³asnego pomys³u.\n";
+	std::cin.get();
+    return 0;
+}
+
